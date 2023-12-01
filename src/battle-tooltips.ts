73ae6756146445fd42d8c857046a2699340f20e5
aurawriter <30663874+aurawriter@users.noreply.github.com>
2023-12-01 15:02:10 -0800

    Update battle-tooltips.ts

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -1099,6 +1099,9 @@ class BattleTooltips {
 		if (ability === 'purepower' || ability === 'hugepower') {
 			stats.atk *= 2;
 		}
+		if (ability === 'enlightenment') {
+			stats.spa *=2;
+		}
 		if (ability === 'hustle' || (ability === 'gorillatactics' && !clientPokemon?.volatiles['dynamax'])) {
 			stats.atk = Math.floor(stats.atk * 1.5);
 		}
